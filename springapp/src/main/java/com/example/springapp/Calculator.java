22cedeab2c9a680d239c9a76ac565af702e90dd6
superdemo09 <superdemo72@gmail.com>
2023-09-11 08:38:04 +0000

    ss

--- springapp/src/main/java/com/example/springapp/Calculator.java
+++ springapp/src/main/java/com/example/springapp/Calculator.java
@@ -4,6 +4,7 @@ public interface Calculator {
 
 //Write Your Code Here
     calculator() {
+        
         System.out.println("dfsa");
-    }
+code-server is being accessed in an insecure context. Web views, the clipboard, and other functional    }
 }
